f2db3befff65dd0ff3cc05b4a15bd5cd31d9258e
larsdeg <54888533+larsdeg@users.noreply.github.com>
2020-06-08 13:32:27 +0200

    Fixed design 1280x720

--- Applicatie/RestaurantChapoo/Hub/Properties/Resources.Designer.cs
+++ Applicatie/RestaurantChapoo/Hub/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Hub.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Knipsel {
+            get {
+                object obj = ResourceManager.GetObject("Knipsel", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
