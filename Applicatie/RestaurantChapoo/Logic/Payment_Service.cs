bf346814ffe6c262bb2007836a9e43653c3beb20
vascomul <54888336+vascomul@users.noreply.github.com>
2020-06-12 22:42:24 +0200

    Final commit
    Afrekenscherm af

--- Applicatie/RestaurantChapoo/Logic/Payment_Service.cs
+++ Applicatie/RestaurantChapoo/Logic/Payment_Service.cs
@@ -13,6 +13,7 @@ namespace Logic
     public class Payment_Service
     {
         private static Payment_Service _uniquePaymentService;
+        private Payment_DAO payment_DAO = new Payment_DAO();
         public static Payment_Service GetPaymentService()
         {
             if (_uniquePaymentService == null)
@@ -30,6 +31,43 @@ namespace Logic
         public void CheckoutOrder()
         {
 
+        }
+
+        public List<int> GetAllReservationID()
+        {
+            List<int> ids = new List<int>();
+            try
+            {
+                DataTable dataTable = payment_DAO.GetAllReservationID();
+                foreach (DataRow dataRow in dataTable.Rows)
+                {
+                    ids.Add(dataRow.Field<int>("ReservationID"));
+                }
+            }
+            catch (Exception e)
+            {
+                
+            }
+            return ids;
+        }
+
+        public List<OrderDetails> GetOrderDetails(int ReservationID)
+        {
+            List<OrderDetails> orderDetails = new List<OrderDetails>();
+            try
+            {
+                DataTable dataTable = payment_DAO.GetOrderDetails(ReservationID);
+                foreach (DataRow dataRow in dataTable.Rows)
+                {
+                    OrderDetails details = new OrderDetails(dataRow.Field<string>("MenuItemName"), dataRow.Field<int>("Amount"), dataRow.Field<decimal>("PricePP"));
+                    orderDetails.Add(details);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return orderDetails;
         }
     }
 }
